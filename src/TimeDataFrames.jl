5c4f19e0487007ba7b30650a243c27f8f148fc12
Michel Juillard <michel.juillard@mjui.fr>
2020-03-05 17:30:10 +0100

    adding methods

--- src/TimeDataFrames.jl
+++ src/TimeDataFrames.jl
@@ -4,13 +4,19 @@ using CSV
 using DataFrames
 using Periods
 
+export TimeDataFrame
+
 struct TimeDataFrame
     data::DataFrame
     continuous::Bool
-    frequency::Periods.Frequency
+    frequency::Frequency
 end;
 
-function TimeDataFrame(dataframe::AbstractDataFrame, frequency::Periods.Frequency, firstperiod)
+TimeDataFrame() = TimeDataFrame(DataFrame(), true, Undated)
+
+TimeDataFrame(frequency::Frequency) = TimeDataFrame(DataFrame(), true, frequency)
+
+function TimeDataFrame(dataframe::AbstractDataFrame, frequency::Frequency, firstperiod)
     data = dataframe
     continuous = true
     data.Periods = [Period(firstperiod + i - 1, 0, Year) for i in 1:dataframe.nrow] 
@@ -23,5 +29,17 @@ function TimeDataFrame(filename::String, frequency::Frequency, firstperiod)
     data.periods = [Period(firstperiod + i - 1, 0, Year) for i in 1:nrow(data)] 
     TimeDataFrame(data, true, frequency) 
 end
+
+# Redefining getproperty breaks tdf.data !
+function Base.getproperty(tdf::TimeDataFrame, symbol::Symbol)
+    data = getfield(tdf, :data)
+    x = getproperty(data, symbol)
+    periods = getproperty(data, :periods)
+    continuous = getfield(tdf, :continuous)
+    frequency = getfield(tdf, :frequency)
+    TimeDataFrame(DataFrame(periods = periods, symbol = x ),  continuous, frequency)
+end
+
+Base.setproperty!(tdf::TimeDataFrame, symbol::Symbol, x::AbstractArray) = setproperty!(getfield(tdf, :data), symbol, x)
 
 end # module
