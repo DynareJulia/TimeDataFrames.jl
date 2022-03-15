e2c80229df61f5efb02412eb57ea9782b3696829
MichelJuillard <michel.juillard@mjui.fr>
2022-03-15 22:21:36 +0100

    remove duplicated functions

--- src/dataframe_functions.jl
+++ src/dataframe_functions.jl
@@ -2,4 +2,3 @@ Base.eachcol(tdf::TimeDataFrame) = eachcol(dataframe(tdf))
 Base.eachrow(tdf::TimeDataFrame) = eachrow(dataframe(tdf))
 ncol(tdf::TimeDataFrame) = DataFrames.ncol(dataframe(tdf))
 nrow(tdf::TimeDataFrame) = DataFrames.nrow(dataframe(tdf))
-
